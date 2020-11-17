270680ad0551604ef77779760c6ad1eed4838fbe
Salman Malik <smalik@pivotal.io>
2020-11-17 11:37:49 -0500

    initial commit

---
+++ src/main/java/com/smalik/esdatafeeder/EsDataFeederApplication.java
@@ -0,0 +1,13 @@
+package com.smalik.esdatafeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsDataFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsDataFeederApplication.class, args);
+	}
+
+}
